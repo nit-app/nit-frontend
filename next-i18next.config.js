3bab90191da957c3228f08be7475fa28ed6a1e52
gaskeo <zeytunyan2004@mail.ru>
2023-09-29 14:47:18 +0500

    chore(config): update eslint, next and i18n configs, add new packages

---
+++ next-i18next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+    debug: true,
+    lng: "ru",
+    nsSeparator: ".",
+    keySeparator: ":",
+    i18n: {
+        localeDetection: false,
+        defaultLocale: "ru",
+        locales: ["ru"],
+    },
+};
