22ebf98bae8e94db278b1eeed899abf8b74cf33c
dowlsss <dowlssss@yandex.by>
2023-10-09 14:16:14 +0500

    api(axios config): the basic configuration of config for working with the api has been carried out

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,7 +2,26 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: process.env["API_BASE_URL"]
+    baseURL: process.env["API_BASE_URL"],
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
+axios.interceptors.request.use(request => {
+    const token = localStorage.getItem("token") || "";
+    request.headers.Authorization = "Bearer " + token;
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 export default instance;
