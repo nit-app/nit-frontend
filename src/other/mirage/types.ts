d78157d26ddf24712e3b416f03944e034c82a3a8
dowlsss <dowlssss@yandex.by>
2023-10-09 14:17:53 +0500

    mock(types): added types for a mock server

---
+++ src/other/mirage/types.ts
@@ -0,0 +1,23 @@
+import { Registry } from "miragejs";
+import Schema from "miragejs/orm/schema";
+
+import { models } from "./models";
+import { factories } from "./factories";
+
+type AppRegistry = Registry<typeof models, typeof factories>
+export type AppSchema = Schema<AppRegistry>
+export type User = {
+    id: string;
+    nickname: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    age: number;
+    sex: string;
+}
+export type Event = {
+    id: string;
+    title: string;
+    description: string;
+    date: Date;
+}
