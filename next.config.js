96c47af9849f7bc392ef298292c156ad1ac5a21d
dowlsss <dowlssss@yandex.by>
2023-10-09 14:14:53 +0500

    mock(old mock): deleted old mock service

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const { i18n } = require("./next-i18next.config");
+const {i18n} = require("./next-i18next.config");
 const nextConfig = {
     i18n,
     reactStrictMode: true,
     pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-    async headers() {
-        return [
-            {
-                source: "/mock.js",
-                headers: process.env["MODE"] === "debug" ? [
-                    {
-                        key: "Service-Worker-Allowed",
-                        value: "/",
-                    },
-                ] : [],
-            },
-        ];
-    },
+    env: {
+        API_BASE_URL: process.env["API_BASE_URL"],
+        USE_MIRAGE_SERVER: process.env["USE_MIRAGE_SERVER"],
+    }
 };
 
 module.exports = nextConfig;
