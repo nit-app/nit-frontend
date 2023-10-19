ace6555395500dd47585bbd78a3d4376b4fd09db
dowlsss <dowlssss@yandex.by>
2023-10-19 14:03:10 +0500

    pages(login page): added login mock page

---
+++ src/pages/login/enterCode.page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { EnterCode } from "@/features/enterCode";
+
+export default function EnterCodePage() {
+    return <EnterCode link="/" phone=""/>;
+};
---
+++ src/pages/login/index.page.tsx
@@ -0,0 +1,7 @@
+import { Login as LoginForm } from "@/features/login";
+
+export default function Login() {
+    return (
+        <LoginForm/>
+    );
+}
